d8282b444d2681a745800e85dff9a02147cd9855
Senuri Wijenayake <senuri.wijenayake@gmail.com>
2020-08-05 20:06:52 +1000

    logs added to all endpoints

--- routes/routes.js
+++ routes/routes.js
@@ -18,6 +18,8 @@ var appRouter = function(app) {
 
   //Endpoint to update user session
   app.post('/updateUserSession', function(req, res) {
+    var x = (req.body.isStart) ? req.body.userId : req.body.sessionId;
+    console.log("At /updateUserSession : Start? - " + req.body.isStart + " " + x);
     return new Promise(function(resolve, reject) {
       logic.updateSession(req.body).then(function(user) {
         resolve(res.status(200).send(user));
@@ -27,6 +29,7 @@ var appRouter = function(app) {
 
   //Endpoint to get user by id
   app.post('/userById', function(req, res) {
+    console.log("At /userById : " + req.body.userId);
     return new Promise(function(resolve, reject) {
       logic.getUserById(req.body.userId).then(function(user) {
         resolve(res.status(200).send(user));
@@ -36,7 +39,7 @@ var appRouter = function(app) {
 
   //Endpoint to check username`available
   app.post('/username', function(req, res) {
-    console.log("Request received at username");
+    console.log("At /username : " + req.body.name);
     return new Promise(function(resolve, reject) {
       logic.checkUsername(req.body.name).then(function(result) {
         resolve(res.status(200).send(result));
@@ -46,7 +49,7 @@ var appRouter = function(app) {
 
   //Endpoint to check email`available
   app.post('/email', function(req, res) {
-    console.log("Request received at email");
+    console.log("At /email : " + req.body.email);
     return new Promise(function(resolve, reject) {
       logic.checkEmail(req.body.email).then(function(result) {
         resolve(res.status(200).send(result));
@@ -77,7 +80,7 @@ var appRouter = function(app) {
 
   //Endpoint to login
   app.post('/login', function(req, res) {
-    console.log("Request received at user login");
+    console.log("At /login : " + req.body.email);
     return new Promise(function(resolve, reject) {
       logic.loginUser(req.body).then(function(obj) {
         if (obj == -1) {
@@ -111,7 +114,7 @@ var appRouter = function(app) {
 
   //Endpoint to get users per group
   app.post('/usergroup', function(req, res) {
-    console.log("Request received at get user group");
+    console.log("At /user group : " + req.body.userId);
     return new Promise(function(resolve, reject) {
       logic.getGroupUsers(req.body).then(function(group) {
         res.status(200).send(group);
@@ -121,7 +124,7 @@ var appRouter = function(app) {
 
   //Endpoint to get votes per question
   app.post('/displayVotes', function(req, res) {
-    console.log("Request received at displayVotes");
+    console.log("At /displayVotes : QuestionID - " + req.body.questionId);
     return new Promise(function(resolve, reject) {
       logic.getVotesForQuestion(req.body).then(function(obj) {
         res.status(200).send(obj);
@@ -131,7 +134,7 @@ var appRouter = function(app) {
 
   //Endpoint to update user status
   app.post('/updateuser', function(req, res) {
-    console.log("Request received at update user");
+    console.log("At /updateuser : " + req.body.userId + " type - " + req.body.type);
     return new Promise(function(resolve, reject) {
       logic.updateUser(req.body).then(function(userId) {
         res.status(200).send(userId);
@@ -141,7 +144,7 @@ var appRouter = function(app) {
 
   //Endpoint to save an answer
   app.post('/userAnswer', function(req, res) {
-    console.log("Request received at userAnswer endpoint");
+    console.log("At /userAnswer : " + req.body.userId + " questionId - " + req.body.questionId);
     return new Promise(function(resolve, reject) {
       //save as user's answer and as a comment
       logic.saveAnswer(req.body).then(function(allComments) {
@@ -154,7 +157,7 @@ var appRouter = function(app) {
 
   //Endpoint to save a reply
   app.post('/saveReply', function(req, res) {
-    console.log("Request received at saveReply endpoint");
+    console.log("At /saveReply : Reply? - " + req.body.isReply + " User : " + req.body.userId + " QuestionId : " + req.body.questionId);
     return new Promise(function(resolve, reject) {
       logic.saveComment(req.body).then(function(allComments) {
         //Retrieve all comments on this question and return
@@ -166,7 +169,7 @@ var appRouter = function(app) {
 
   //Endpoint to update vote count for a given comment
   app.post('/updateVoteForComment', function(req, res) {
-    console.log("Request received at updateVoteForComment endpoint");
+    console.log("At /updateVoteForComment : User - " + req.body.userId + " Comment : " + req.body.commentId);
     return new Promise(function(resolve, reject) {
       logic.updateVoteForComment(req.body).then(function(allComments) {
         //Retrieve all comments on this question and return
@@ -178,7 +181,7 @@ var appRouter = function(app) {
 
   //Endpoint to get all the questions and answers
   app.post('/questions', function(req, res) {
-    console.log("Request received at all questions");
+    console.log("At /questions : " + req.body.userId);
     return new Promise(function(resolve, reject) {
       logic.getAllQuestions(req.body).then(function(questions) {
         //Retrieve all comments on this question and return
@@ -190,7 +193,7 @@ var appRouter = function(app) {
 
   //Endpoint to get all the questions and votes
   app.post('/questionsAtVote', function(req, res) {
-    console.log("Request received at questionsAtVote");
+    console.log("At /questionsAtVote : " + req.body.userId);
     return new Promise(function(resolve, reject) {
       logic.getAllQuestionsToVote(req.body).then(function(questions) {
         result = JSON.stringify(questions);
@@ -201,7 +204,7 @@ var appRouter = function(app) {
 
   //Endpoint to get all questions answered by a given user
   app.post('/questionsPerUser', function(req, res) {
-    console.log("Request received at all questionsPerUser");
+    console.log("At /questionsPerUser : " + req.body.userId);
     return new Promise(function(resolve, reject) {
       logic.getAnswersByUser(req.body).then(function(questions) {
         result = JSON.stringify(questions);
@@ -212,7 +215,7 @@ var appRouter = function(app) {
 
   //Endpoint to get all topics voted by a auser
   app.post('/votesPerUser', function(req, res) {
-    console.log("Request received at votesPerUser");
+    console.log("At /votesPerUser : " + req.body.userId);
     return new Promise(function(resolve, reject) {
       logic.getVotesByUser(req.body).then(function(questionsVoted) {
         result = JSON.stringify(questionsVoted);
@@ -223,7 +226,7 @@ var appRouter = function(app) {
 
   //Endpoint to get all comments for the question
   app.post('/userComments', function(req, res) {
-    console.log("Request received at get user comments for question");
+    console.log("At get /userComments : Question - " + req.body.questionId + " User - " + req.body.userId);
     return new Promise(function(resolve, reject) {
       logic.getCommentsForQuestion(req.body).then(function(group) {
         res.status(200).send(group);
@@ -242,19 +245,21 @@ var appRouter = function(app) {
 
   //Endpoint to get the big five questions
   app.get('/bigFiveQuestions', function(req, res) {
+    console.log("At /bigFiveQuestions");
     data = logic.getBigFiveQuestions();
     res.status(200).send(data);
   });
 
   //Endpoint to get the UES questions
   app.get('/UESQuestions', function(req, res) {
+    console.log("At /UESQuestions");
     data = logic.getUESQuestions();
     res.status(200).send(data);
   });
 
   //Endpoint to process the UES data
   app.post('/engagementSurvey', function(req, res) {
-    console.log("Request received at engagementSurvey");
+    console.log("At /engagementSurvey : " + req.body.userId);
     return new Promise(function(resolve, reject) {
       var data = {
         userId: req.body.userId,
@@ -272,7 +277,7 @@ var appRouter = function(app) {
 
   //Endpoint to process the big five data
   app.post('/bigFiveData', function(req, res) {
-    console.log("Request received at big five");
+    console.log("At /bigFiveData : " + req.body.userId);
     var code = req.body.userId + "_" + Date.now();
     return new Promise(function(resolve, reject) {
       var data = {
@@ -290,7 +295,7 @@ var appRouter = function(app) {
 
   //Endpoint to update answer
   app.post('/updateAnswer', function(req, res) {
-    console.log("Request received at update answer");
+    console.log("At /updateAnswer : User - " + req.body.userId + " QuestionId : " + req.body.questionId);
     return new Promise(function(resolve, reject) {
       logic.updateAnswer(req.body).then(function(id) {
         resolve(res.status(200).send(id));
